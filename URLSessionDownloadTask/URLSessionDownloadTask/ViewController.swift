2564211248c29dd20521e8f7e5d7b7719cceebed
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-04-07 08:14:34 +0200

    Initial File setup

---
+++ URLSessionDownloadTask/URLSessionDownloadTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionDownloadTask
+//
+//  Created by Bonginkosi Tshabalala on 2023/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
